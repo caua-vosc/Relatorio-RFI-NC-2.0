eddd21c1af8d45dfea1e00bac55d3a7ac728a5be
caua-vosc <caua_viniciusosc12@outlook.com>
2026-01-14 15:41:30 -0300

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,6 +1,6 @@
 async function uploadParaNextcloud(siteId, state){
     const response = await fetch(
-        "https://relatorio-rfi-nc-2-0-9d9hdmc47-caua-voscs-projects.vercel.app/api/upload",
+        "https://relatorio-rfi-nc-2-0-hmxntxmuz-caua-voscs-projects.vercel.app/api/upload",
         {
             method: "POST",
             headers: { "Content-Type": "application/json" },
