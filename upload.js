45e74b799ef3025d7eb088fdc575ddc861ddad1c
caua-vosc <caua_viniciusosc12@outlook.com>
2026-01-14 23:04:28 -0300

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,18 +1,33 @@
 async function uploadParaNextcloud(siteId, state) {
 
+  if (!siteId || !state) {
+    throw new Error("Dados inválidos antes do envio");
+  }
+
   const ENDPOINT =
     "https://rfi-20.caua-viniciusosc12.workers.dev/";
+
+  const payload = {
+    siteId: siteId,
+    state: state
+  };
+
+  console.log("ENVIANDO:", payload);
 
   const res = await fetch(ENDPOINT, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ siteId, state })
+    body: JSON.stringify(payload)
   });
 
-  if (!res.ok)
-    throw new Error(await res.text());
+  const txt = await res.text();
+  console.log("RESPOSTA WORKER:", txt);
 
-  return res.json();
+  if (!res.ok) {
+    throw new Error(txt);
+  }
+
+  return JSON.parse(txt);
 }
